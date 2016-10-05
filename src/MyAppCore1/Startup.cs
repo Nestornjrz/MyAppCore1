e481deb89c5c4ddebda648373075cbd2d687a1cd
Nestornjrz <nestornjrz@hotmail.com>
2016-10-05 14:13:30 -0300

    Cambios hechos para agregar archivo de configuracion

--- src/MyAppCore1/Startup.cs
+++ src/MyAppCore1/Startup.cs
@@ -7,11 +7,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace MyAppCore1
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env) {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration =  builder.Build();
+        }
+        public IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -30,7 +39,8 @@ namespace MyAppCore1
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var message = Configuration["Saludo"];
+                await context.Response.WriteAsync(message);
             });
         }
     }
