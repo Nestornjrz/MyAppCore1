c91d974deb937b5798fbc938c832e49b213a160f
Nestornjrz <nestornjrz@hotmail.com>
2016-10-07 14:31:47 -0300

    Uso basico de los atributos de ruteo

--- src/MyAppCore1/Controllers/AboutController.cs
+++ src/MyAppCore1/Controllers/AboutController.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyAppCore1.Controllers
 {
+    [Route("about")]
     public class AboutController
     {
+        [Route("")]
         public string Telefono() {
             return "0981 263.726";
         }
 
+        [Route("direccion")]
         public string Direccion() {
             return "Paraguay";
         }
