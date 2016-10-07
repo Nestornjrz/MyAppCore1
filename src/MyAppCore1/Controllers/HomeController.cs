dbcbe3ffb41642b3f79f740497573b9391566f1e
Nestornjrz <nestornjrz@hotmail.com>
2016-10-07 16:21:36 -0300

    Enviando contenido desde el metodo Content de la interfaz IActionResult

--- src/MyAppCore1/Controllers/HomeController.cs
+++ src/MyAppCore1/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAppCore1.Models;
 
-namespace MyAppCore1.Controllers
-{
-    public class HomeController
+namespace MyAppCore1.Controllers {
+    public class HomeController:Controller
     {
-        public string Index() {
-            return "Hola, desde el HomeController";
+        public IActionResult Index() {               
+            return Content("Hola, desde el HomeController");
         }
     }
 }
