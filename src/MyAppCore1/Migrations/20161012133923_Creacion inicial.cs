81d3d02ec7c8d5001c197ca9a2ed6c36705bfcd8
Nestornjrz <nestornjrz@hotmail.com>
2016-10-12 11:28:31 -0300

    Creamos la base de datos por medio del comando update-database en la Consola del Administrador de paquetes

---
+++ src/MyAppCore1/Migrations/20161012133923_Creacion inicial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MyAppCore1.Migrations
+{
+    public partial class Creacioninicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cosina = table.Column<int>(nullable: false),
+                    Nombre = table.Column<string>(maxLength: 80, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurantes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurantes");
+        }
+    }
+}
---
+++ src/MyAppCore1/Migrations/MyAppCore1DbContextModelSnapshot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MyAppCore1.Entities;
+
+namespace MyAppCore1.Migrations
+{
+    [DbContext(typeof(MyAppCore1DbContext))]
+    partial class MyAppCore1DbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MyAppCore1.Entities.Restaurante", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Cosina");
+
+                    b.Property<string>("Nombre")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 80);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Restaurantes");
+                });
+        }
+    }
+}
