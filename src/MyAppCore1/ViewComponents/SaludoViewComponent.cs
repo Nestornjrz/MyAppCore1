784ec825e61b5f761655149bda762138d93dcbda
Nestornjrz <nestornjrz@hotmail.com>
2016-10-13 15:42:50 -0300

    Utilizamos el WiewComponent

---
+++ src/MyAppCore1/ViewComponents/SaludoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAppCore1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAppCore1.ViewComponents
+{
+    public class SaludoViewComponent: ViewComponent
+    {
+        private ISaludo _saludo;
+        public SaludoViewComponent(ISaludo saludo) {
+            _saludo = saludo;
+        }
+        public IViewComponentResult Invoke() {
+            var model = _saludo.GetSaludo();
+            return View("Default",model);
+        }
+    }
+}
