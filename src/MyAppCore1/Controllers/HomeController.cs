a1199106cb5ff0abd1582ee2584b6c1b753b5ff8
Nestornjrz <nestornjrz@hotmail.com>
2016-10-14 09:42:35 -0300

    Uso basico de los atributos Authorize y AllowAnonymous.

--- src/MyAppCore1/Controllers/HomeController.cs
+++ src/MyAppCore1/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MyAppCore1.Entities;
 using MyAppCore1.Services;
 using MyAppCore1.ViewModels;
 
 
 namespace MyAppCore1.Controllers {
+    [Authorize]
     public class HomeController : Controller {
         private IRestauranteData _restauranteData;
         private ISaludo _saludo;
@@ -13,13 +15,14 @@ namespace MyAppCore1.Controllers {
             _restauranteData = restauranteData;
             _saludo = saludo;
         }
+        [AllowAnonymous]
         public IActionResult Index() {
             var model = new HomePageViewModel();
             model.Restaurantes = _restauranteData.GetAll();
             model.MensajeActual = _saludo.GetSaludo();
 
             return View(model);
-        }
+        }       
         public IActionResult Details(int id) {
             var model = _restauranteData.Get(id);
             if (model == null) {
