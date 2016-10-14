0cdeb1d8e682a6c371e204b4aea03aab5f6a4908
Nestornjrz <nestornjrz@hotmail.com>
2016-10-14 15:52:26 -0300

    Preparando para registrar el usuario

---
+++ src/MyAppCore1/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAppCore1.ViewModels;
+
+namespace MyAppCore1.Controllers
+{
+    public class AccountController:Controller
+    {
+        [HttpGet]
+        public IActionResult Register() {
+            return View();
+        }
+
+    }
+}
---
+++ src/MyAppCore1/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyAppCore1.ViewModels {
+    public class RegisterUserViewModel
+    {
+        [Required,MaxLength(256)]
+        public string Username { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password),Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
