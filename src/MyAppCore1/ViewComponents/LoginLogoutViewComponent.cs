ec6f9276a64e5cb77f9a99078f303c671a64607a
Nestornjrz <nestornjrz@hotmail.com>
2016-10-17 11:03:18 -0300

    Se agrega el componente LoginLogout y se prepara el formulario para poder desloguearse.

---
+++ src/MyAppCore1/ViewComponents/LoginLogoutViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAppCore1.ViewComponents {
+    public class LoginLogoutViewComponent : ViewComponent {
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
