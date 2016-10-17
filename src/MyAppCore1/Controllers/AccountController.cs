f37e4f99889c0698b287e535f1e0f2ba38116f8f
Nestornjrz <nestornjrz@hotmail.com>
2016-10-17 14:09:05 -0300

    Implementacion del login

--- src/MyAppCore1/Controllers/AccountController.cs
+++ src/MyAppCore1/Controllers/AccountController.cs
@@ -4,10 +4,8 @@ using MyAppCore1.Entities;
 using MyAppCore1.ViewModels;
 using System.Threading.Tasks;
 
-namespace MyAppCore1.Controllers
-{
-    public class AccountController:Controller
-    {
+namespace MyAppCore1.Controllers {
+    public class AccountController : Controller {
         private SignInManager<User> _gerenciaDeLogeo;
         private UserManager<User> _gerenciaDeUsuario;
 
@@ -25,13 +23,13 @@ namespace MyAppCore1.Controllers
         public async Task<IActionResult> Register(RegisterUserViewModel model) {
             if (ModelState.IsValid) {
                 var user = new User { UserName = model.Username };
-                var createResul  =await _gerenciaDeUsuario.CreateAsync(user, model.Password);
+                var createResul = await _gerenciaDeUsuario.CreateAsync(user, model.Password);
                 if (createResul.Succeeded) {
                     await _gerenciaDeLogeo.SignInAsync(user, false);
                     return RedirectToAction("Index", "Home");
                 } else {
                     foreach (var error in createResul.Errors) {
-                        ModelState.AddModelError("",error.Description);
+                        ModelState.AddModelError("", error.Description);
                     }
                 }
             }
@@ -40,7 +38,30 @@ namespace MyAppCore1.Controllers
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Logout() {
             await _gerenciaDeLogeo.SignOutAsync();
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Login() {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model) {
+            if (ModelState.IsValid) {
+                var loginResult = await _gerenciaDeLogeo.PasswordSignInAsync(
+                                       model.Username,
+                                       model.Password,
+                                       model.RememberMe, false);
+                if (loginResult.Succeeded) {
+                    if (Url.IsLocalUrl(model.ReturnUrl)) {
+                        return Redirect(model.ReturnUrl);
+                    }else {
+                        return RedirectToAction("Index","Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("","No se puede loguear");
+            return View(model);
         }
     }
 }
